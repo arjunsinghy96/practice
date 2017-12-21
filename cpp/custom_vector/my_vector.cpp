c0380bce2d1dd4b28cc6aab3b9d87ce86b41535a
arjunsinghy96 <14bcs010@smvdu.ac.in>
2017-12-21 23:45:50 +0530

    Complete my_vector.cpp
    Adds additional functions to the Vector class

--- cpp/custom_vector/my_vector.cpp
+++ cpp/custom_vector/my_vector.cpp
@@ -34,14 +34,93 @@ class Vector{
         }
 
         void print(){
-            if(this->v_size == 0){
+            if(v_size == 0){
                 cout<< "[]" << endl;
                 return;
             }
             cout << "[";
-            for(int i =0; i<this->v_size-1; i++){
-                cout << *(this->arr + i) << ", ";
+            for(int i =0; i<v_size-1; i++){
+                cout << *(arr + i) << ", ";
             }
-            cout << *(this->arr + this->v_size)<< "]"<<endl;
+            cout << *(arr + v_size-1)<< "]"<<endl;
+        }
+
+        bool is_empty(){
+            return v_size == 0;
+        }
+
+        int at(int index){
+            if(index < v_size){
+                return *(arr + index);
+            }
+            cout << "IndexOutOfBounds\n" << endl;
+            exit(-1);
+        }
+
+        void push(int value){
+            if(v_size == v_capacity){
+                _extend_vector();
+            }
+            *(arr + v_size) = value;
+            v_size += 1;
+        }
+
+        void insert(int index, int value){
+            int temp, temp2=value , i;
+            if(v_size == v_capacity){
+                _extend_vector();
+            }
+            if(index < v_size){
+                for(i=index; i<v_size; i++){
+                    temp = *(arr + i);
+                    *(arr + i) = temp2;
+                    temp2 = temp;
+                }
+                *(arr + v_size) = temp2;
+                v_size +=1;
+            }
+        }
+
+        int pop(){
+            if(v_size == 0){
+                cout << "ArrayEmptyError\n";
+                exit(-1);
+            }
+            v_size -=1;
+            int ret_val = *(arr + v_size);
+            if(v_size < v_capacity/4){
+                _shrink_vector();
+            }
+            return ret_val;
+        }
+
+        void del(int index){
+            if(v_size > index){
+                for(int i=index; i< v_size -1; i++) {
+                    *(arr + i) = *(arr + i + 1);
+                }
+                v_size -= 1;
+            }
+            else {
+                cout << "IndexOutOfBounds\n";
+                exit(-1);
+            }
+        }
+
+        int find(int value){
+            for(int i=0; i< v_size; i++) {
+                if(*(arr + i) == value) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        int& operator[] (int index) {
+            if(index > v_size) {
+                cout << "IndexOutOfBounds\n";
+                exit(-1);
+            }
+            return arr[index];
         }
 };
--- cpp/custom_vector/test.cpp
+++ cpp/custom_vector/test.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
+#include<assert.h>
 #include"my_vector.cpp"
 
 using namespace std;
 
 int main(){
     Vector v;
-    cout << v.size() << endl;
-    cout << v.capacity()<< endl;
+    assert(v.size() == 0);
+    assert(v.capacity() == 16);
+    for(int i=0;i<20; i++){
+        v.push(2*i);
+    }
+    assert(v.size() == 20);
+    assert(v.capacity() == 32);
     v.print();
+    v.del(10);
+    assert(v.size() == 19);
+    v.insert(5, 100);
+    v.print();
+    for(int i=0; i<17; i++){
+        v.pop();
+    }
+    v.print();
+    assert(v.capacity() == 8);
     return 0;
 }
